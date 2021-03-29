54972d5f021938f31f2055fc6f97300badd8f9aa
Preheated-Oven <78744706+Preheated-Oven@users.noreply.github.com>
2021-03-29 15:27:05 -0500

    Add files via upload

---
+++ Timing_Circuit_Project.R
@@ -0,0 +1,11 @@
+#The goal of this project is to annalyze a timing circuit in order to
+#accurately calculate its frequency. This project has two main goals:
+
+#Firstly, to connect a timing circuit to a Rasperry Pi so that it can
+#interpret the inputs from the circuit.
+
+#Secondly, to design a software capable of tracking the inputs and
+#performing the needed calculations.
+
+#The first goal is being accomplished by Casey Cox. He has acquired the
+#Needed circuit, and is working to connect it to the Pi.
